1320ea77e34a01388e338827a2cc265860ba61d0
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2024-01-04 19:17:47 +0530

    api changes error success message

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const Medicine  = require("./models/medicine.js");
+const Medicine = require("./models/medicine.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+
+const success_status = { status: 200, message: "Success" };
+const error_status = { status: 404, message: "Something went Wrong" };
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,52 +17,50 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-const connectDB = async ()=>{
-    try{
+const connectDB = async () => {
+    try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-    }catch (error){
+    } catch (error) {
         console.log(error);
         process.exit(1);
     }
 }
 
-app.get("/",(req,res)=>{
-    res.send({medicine:'Medicine'})
+
+app.get("/", (req, res) => {
+    res.send({ medicine: 'Medicine' })
 })
 
-app.post("/addMedicine",async (req,res)=>{
+app.post("/addMedicine", async (req, res) => {
     try {
         console.log(req.body);
         const data = req.body.medicList;
         await Medicine.insertMany(data);
-         res.send("Data added");
+        res.send(success_status);
     } catch (error) {
-        res.send("Error Caused")
-        console.log("err",error);
+        res.send(error_status);
+        console.log("err", error);
     }
-   
+
 })
 
-app.get("/getStockDetail",async(req,res)=>{
+app.get("/getStockDetail", async (req, res) => {
     const medic = await Medicine.find();
     medic ? res.json(medic) : res.send("Wrong acces");
 })
 
-app.put("/updateStock",async(req,res)=>{
+app.put("/updateStock", async (req, res) => {
     try {
         const data = req.body;
-              await Medicine.findByIdAndUpdate(data._id,data);
-    //    await data.forEach((e)=>{
-    //     console.log(e);
-    //     })
-        res.send("Data updated")
+        await Medicine.findByIdAndUpdate(data._id, data);
+        res.send(success_status);
 
     } catch (error) {
         console.log(error);
-        res.send("Data Erorr")
+        res.send(error_status);
     }
 })
 
-connectDB().then(()=>{
-    app.listen(PORT,()=>console.log("Connected and listeing ",PORT));
+connectDB().then(() => {
+    app.listen(PORT, () => console.log("Connected and listeing ", PORT));
 })
